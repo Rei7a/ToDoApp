820e921453375d2bf639d35d9e036606cc73c924
Matsuda_Reita <53333235+Rei7a@users.noreply.github.com>
2019-09-04 01:16:52 +0900

    a
    a
    
    Co-Authored-By: Matsuda_Reita <rei7a@users.noreply.github.com>

---
+++ src/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+//aterial-UI//
+
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+function App() {
+  return (
+    <Button >
+      aaa
+    </Button>
+  );
+}
+
+export default App;
